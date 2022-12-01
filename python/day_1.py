c1e223e2fd5d70ba0988b228f94acc489d574921
Oleg Kainov <kainov.oleg@gmail.com>
2022-12-01 19:55:11 +0100

    day 1: add part 2

--- python/day_1.py
+++ python/day_1.py
@@ -3,17 +3,17 @@ import os
 if __name__ == '__main__':
     result = 0
     with open(os.path.join('..', 'day_1_input.txt'), 'r') as f:
+        elves_cal = []
         current_elf_cal = 0
-        max_cal = 0
         for line in f:
             if not line.strip():
-                if max_cal < current_elf_cal:
-                    max_cal = current_elf_cal
+                elves_cal.append(current_elf_cal)
                 current_elf_cal = 0
                 continue
             current_elf_cal += int(line)
 
-    print(max_cal)
+    print(max(elves_cal))
+    print(sum(sorted(elves_cal)[-3:]))
 
     # First part answer:  70613
-    # Second part answer:
+    # Second part answer: 205805
