d3cfcdb9daa41d3162349e462a370e42617e2644
Oleg Kainov <oleg.kainov@intel.com>
2022-12-03 11:13:49 +0100

    day 3: add part 2

--- python/day_3.py
+++ python/day_3.py
@@ -1,11 +1,34 @@
 import os
 
+
+def get_priority(c):
+    if c.islower():
+        return ord(c) - 96
+    else:
+        return ord(c) - 65 + 27
+
+
 if __name__ == '__main__':
     result = 0
+    result_2 = 0
 
     with open(os.path.join('..', 'day_3_input.txt'), 'r') as f:
-        for line in f:
+        group_items = {}
+        for i, line in enumerate(f):
             line = line.strip()
+
+            for c in set(line):
+                if c not in group_items:
+                    group_items[c] = 0
+                group_items[c] += 1
+            if i % 3 == 2:
+                # Find common item: the one with counter 3
+                for key, value in group_items.items():
+                    if value == 3:
+                        result_2 += get_priority(key)
+                # End of line, cleanup
+                group_items = {}
+
             backpack_size = len(line) // 2
             backpack = {}
             for c in line[:backpack_size]:
@@ -13,16 +36,13 @@ if __name__ == '__main__':
             for c in line[backpack_size:]:
                 if c in backpack:
                     # Found duplicate
-                    score = 0
-                    if c.islower():
-                        score = ord(c) - 96
-                    else:
-                        score = ord(c) - 65 + 27
-                    print(f'Dup is {c} ({score})')
+                    score = get_priority(c)
+                    # print(f'Dup is {c} ({score})')
                     result += score
                     break
 
     print(result)
+    print(result_2)
 
     # First part answer:  7875
-    # Second part answer:
+    # Second part answer: 2479
