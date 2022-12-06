d56093584030d527c27fcc9da84666d3359d406e
Oleg Kainov <oleg.kainov@intel.com>
2022-12-06 15:58:37 +0530

    day 6: add part 2

--- python/day_6.py
+++ python/day_6.py
@@ -1,18 +1,18 @@
 import os
 
 if __name__ == '__main__':
+    LENGTH = 14 # Set 4 for part 1
     with open(os.path.join('..', 'day_6_input.txt'), 'r') as f:
         for line in f:
             stack = []
             for i, c in enumerate(line, start=1):
                 # Remove first char
-                if len(stack) == 4:
+                if len(stack) == LENGTH:
                     stack = stack[1:]
                 stack.append(c)
-                if len(stack) == len(set(stack)) and len(stack) == 4:
+                if len(stack) == len(set(stack)) and len(stack) == LENGTH:
                     print(i)
                     break
 
-
     # First part answer: 1578
-    # Second part answer:
+    # Second part answer: 2178
